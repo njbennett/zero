385cbf023d8df1271515310d10e0205957286ea8
Nat Bennett <natalie.j.bennett@gmail.com>
2023-03-17 13:56:18 -0700

    Store and return creator values

--- lib/zero/filter.ex
+++ lib/zero/filter.ex
@@ -8,4 +8,9 @@ defmodule Zero.Filter do
   def creator do
     Agent.get(__MODULE__, & &1)
   end
+
+  def creator(value) do
+    Agent.update(__MODULE__, fn(_) -> value end)
+    value
+  end
 end
--- test/zero/filter_test.exs
+++ test/zero/filter_test.exs
@@ -4,8 +4,21 @@ defmodule Zero.FilterTest do
 
   describe "filter" do
 
+    setup do
+      {:ok, filter: Filter.creator("")}
+    end
+
     test "returns its starting value" do
       assert Filter.creator() == ""
+    end
+
+    test "returns values that have been set" do
+      assert Filter.creator("Edgar") == "Edgar"
+    end
+
+    test "persists values" do
+      Filter.creator("Edgar")
+      assert Filter.creator() == "Edgar"
     end
   end
 end
