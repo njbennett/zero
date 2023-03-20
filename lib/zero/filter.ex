6871c157b5d1f697e8ef20c64a5f932971c15434
Nat Bennett <natalie.j.bennett@gmail.com>
2023-03-19 18:27:36 -0700

    Add "creator_as" functions for getting and storing creators by user

--- lib/zero/filter.ex
+++ lib/zero/filter.ex
@@ -10,7 +10,16 @@ defmodule Zero.Filter do
   end
 
   def creator(value) do
-    Agent.update(__MODULE__, &Map.put(&1, "", value))
+    :ok = Agent.update(__MODULE__, &Map.put(&1, "", value))
     value
+  end
+
+  def creator_as(as, creator) do
+    :ok = Agent.update(__MODULE__, &Map.put(&1, as, creator))
+    creator
+  end
+
+  def creator_as(as) do
+    Agent.get(__MODULE__, &Map.get(&1, as))
   end
 end
--- test/zero/filter_test.exs
+++ test/zero/filter_test.exs
@@ -7,17 +7,35 @@ defmodule Zero.FilterTest do
       {:ok, filter: Filter.creator("")}
     end
 
-    test "returns its starting value" do
+    test "creator/0 returns its starting value" do
       assert Filter.creator() == ""
     end
 
-    test "returns values that have been set" do
+    test "creator/1 returns values that have been set" do
       assert Filter.creator("Edgar") == "Edgar"
     end
 
-    test "persists values" do
+    test "creator/1 persists values" do
       Filter.creator("Edgar")
       assert Filter.creator() == "Edgar"
+    end
+
+    test "creator_as/2 can store multiple values" do
+      assert Filter.creator_as("Edgar", "Edgar") == "Edgar"
+    end
+
+    test "creator_as/1 returns the creator set for the provided key" do
+      Filter.creator_as("Edgar", "Edgar")
+      assert Filter.creator_as("Edgar") == "Edgar"
+    end
+
+    test "creator_as/2 can set creator to a different value than the key" do
+      assert Filter.creator_as("Edgar", "Ollie") == "Ollie"
+    end
+
+    test "creator_as/2 persists values" do
+      Filter.creator_as("Edgar", "Ollie")
+      assert Filter.creator_as("Edgar") == "Ollie"
     end
   end
 end
