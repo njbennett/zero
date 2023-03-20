4a8f4d7b862f8d5658640a4b162411e8be660a48
Nat Bennett <natalie.j.bennett@gmail.com>
2023-03-19 18:23:41 -0700

    Store creators in a map, so we can store different creators

--- lib/zero/filter.ex
+++ lib/zero/filter.ex
@@ -2,15 +2,15 @@ defmodule Zero.Filter do
   use Agent
 
   def start_link(_) do
-    Agent.start_link(fn -> "" end, name: __MODULE__)
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
   end
 
   def creator do
-    Agent.get(__MODULE__, & &1)
+    Agent.get(__MODULE__, &Map.get(&1, ""))
   end
 
   def creator(value) do
-    Agent.update(__MODULE__, fn _ -> value end)
+    Agent.update(__MODULE__, &Map.put(&1, "", value))
     value
   end
 end
